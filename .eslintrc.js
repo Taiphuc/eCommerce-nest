dad1a828351d21066a64287f668d22a8d8e6b315
unknown <el.taiphuc123@gmail.com>
2024-05-29 15:55:27 +0700

    explain .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,26 @@
 module.exports = {
+  // cấu hình parser
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
+  // cấu hình các plugin
   plugins: ['@typescript-eslint/eslint-plugin'],
+  // mở rộng các bộ quy tắc có sẵn
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   root: true,
+  // môi trường mã sẽ chạy
   env: {
     node: true,
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
+  // định nghĩa quy tắc tùy chỉnh
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
